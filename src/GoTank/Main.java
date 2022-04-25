e3c2cab586b7a0629801e504abcdc14f11e6227f
FlagmanZed <degraff@bk.ru>
2022-04-26 00:37:55 +0300

    Commit 1

--- src/GoTank/Main.java
+++ src/GoTank/Main.java
@@ -12,11 +12,20 @@ with fuel and different constructors
         // At (0;0) fuel=100
         Tank justTank = new Tank();
         // At (10;10) fuel=100
+        System.out.println(justTank.fuel);
         Tank anywareTank = new Tank(10, 10);
         // At (20;30) fuel=200
         Tank customTank = new Tank(20, 30, 200);
-        justTank.goForward(200);
+        justTank.goForward(50);
         justTank.printPosition();
+        justTank.turnLeft();
+        justTank.goForward(30);
+        justTank.printPosition();
+        justTank.turnLeft();
+        justTank.goForward(50);
+        justTank.printPosition();
+        System.out.println(justTank.fuel);
+
         anywareTank.goBackward(-200);
         anywareTank.printPosition();
         customTank.goForward(201);
