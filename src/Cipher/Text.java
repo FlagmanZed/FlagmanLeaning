9d84e52174439401b24d5c294d6d7387f5364f76
FlagmanZed <degraff@bk.ru>
2022-06-03 19:33:12 +0300

    Все еще надо доработать: использовать вложенные классы

--- src/Cipher/Text.java
+++ src/Cipher/Text.java
@@ -6,10 +6,11 @@ import java.util.Scanner;
 public class Text {
 
     private int[] arrSumWords, arrSumChar;
+    private boolean isStop;
 
     // преобразуем текст в массивы
-    void codeText(String txtForNum) {
-        String[] text = txtForNum.trim().split(" ");
+    void codeText(String message) {
+        String[] text = message.trim().split(" ");
         arrSumWords = new int[text.length];
         int ch = 0, w = 0, wrdlgth = 0;
         for (String tm : text) {
@@ -23,19 +24,6 @@ public class Text {
                 arrSumChar[i + wrdlgth] = wrd.charAt(i);
             }
             wrdlgth += wrd.length();
-        }
-    }
-
-    void ciphText(int[] arrSumChar) {
-        for (int i = 0; i < arrSumChar.length; i++){
-            arrSumChar[i] = arrSumChar[i]*2-arrSumChar.length-i;
-        }
-    }
-
-    void deciphText(String txtForDeciph) {
-        codeText(txtForDeciph);
-        for (int i = 0; i < arrSumChar.length; i++){
-            arrSumChar[i] = (arrSumChar[i]+arrSumChar.length+i)/2;
         }
     }
 
@@ -56,23 +44,61 @@ public class Text {
         return tmp.toString().trim();
     }
 
+    // преобразуем массив в текст
+    int[] convers(int[] arr) {
+        for (int i = 0; i < arr.length / 2; i++) {
+            int tmp = arr[i];
+            arr[i] = arr[arr.length - 1 - i];
+            arr[arr.length - 1 - i] = tmp;
+        }
+        return arr;
+    }
+
+    void ciphText(String txtForCiph) {
+        codeText(txtForCiph);
+        for (int i = 0; i < arrSumChar.length; i++) {
+            arrSumChar[i] = arrSumChar[i] * 2 - arrSumChar.length + i;
+        }
+        arrSumWords = convers(arrSumWords);
+        System.out.println(printText(arrSumWords, arrSumChar));
+        System.out.println(Arrays.toString(arrSumChar));
+        System.out.println(Arrays.toString(arrSumWords));
+    }
+
+    void deciphText(String txtForDeciph) {
+        codeText(txtForDeciph);
+        for (int i = 0; i < arrSumChar.length; i++) {
+            arrSumChar[i] = (arrSumChar[i] + arrSumChar.length - i) / 2;
+        }
+        arrSumWords = convers(arrSumWords);
+        System.out.println(printText(arrSumWords, arrSumChar));
+    }
+
+    void work() {
+        isStop = false;
+        String message = "";
+        Scanner scan = new Scanner(System.in);
+        Scanner mess = new Scanner(System.in);
+        System.out.println("""
+                        Выберите действие:
+                '1'             - зашифровать сообщение
+                '2'             - расшифровать сообщение
+                'Другой символ' - выход""");
+        char symbol = scan.next().charAt(0);
+        if (symbol == '1') {
+            message = mess.nextLine();
+            ciphText(message);
+        } else if (symbol == '2') {
+            message = mess.nextLine();
+            deciphText(message);
+        } else isStop = true;
+    }
 
     public static void main(String[] args) {
 
-        Scanner scan = new Scanner(System.in);
-        String txtForCiph = scan.nextLine();
         Text text = new Text();
-
-        text.codeText(txtForCiph);
-        text.ciphText(text.arrSumChar);
-        System.out.println(text.printText(text.arrSumWords, text.arrSumChar));
-        System.out.println(Arrays.toString(text.arrSumChar));
-        System.out.println(Arrays.toString(text.arrSumWords));
-
-        String txtForDeciph = scan.nextLine();
-        text.deciphText(txtForDeciph);
-        System.out.println(text.printText(text.arrSumWords, text.arrSumChar));
-        System.out.println(Arrays.toString(text.arrSumChar));
-        System.out.println(Arrays.toString(text.arrSumWords));
+        while (!text.isStop) {
+            text.work();
+        }
     }
 }
